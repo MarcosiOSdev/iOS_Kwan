7f6637dd04699f89e75c9bf3fde77c91bf62a097
Marcos Felipe Souza <mfelipesp@gmail.com>
2019-05-23 21:15:30 -0300

    Initing project

---
+++ Kwan Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kwan Challenge
+//
+//  Created by Marcos Felipe Souza on 23/05/19.
+//  Copyright © 2019 Marcos Felipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
